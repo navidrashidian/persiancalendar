1526e22ffee22a0275e12748cb2fdc34df77bea2
Navid Rashidian <navidrashidian@gmail.com>
2025-07-01 16:10:49 +0330

    Add ``extern "C"`` for C++ linkage

--- persiancalendar_fast.h
+++ persiancalendar_fast.h
@@ -1,6 +1,10 @@
 #ifndef PERSIAN_CALENDAR
 #define PERSIAN_CALENDAR
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 extern const int SUPPORTED_FIRST_YEAR;
 extern const int SUPPORTED_LAST_YEAR;
 
@@ -19,5 +23,9 @@ typedef struct {
 int fixed_from_gregorian(GregorianDate g_date);
 int fixed_from_persian_fast(PersianDate p_date);
 PersianDate persian_fast_from_fixed(int date);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif
